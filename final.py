83b41c2786cf093bf87162807d208edee8ad5996
Ryan McClelland <rymcclel@gmail.com>
2019-11-21 02:05:14 -0800

    add flow

--- final.py
+++ final.py
@@ -2,8 +2,23 @@ import cv2
 import numpy as np
 import imutils
 
+# Create some random colors
+color = np.random.randint(0,255,(100,3))
 
-cap = cv2.VideoCapture('Study clip 017.mpg')
+first = True
+
+# params for ShiTomasi corner detection
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+
+# Parameters for lucas kanade optical flow
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+cap = cv2.VideoCapture('Study clip 057.mpg')
 ret, frame = cap.read()
 frameCount = 0
 
@@ -13,13 +28,11 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 fps = cap.get(cv2.CAP_PROP_FPS)
 
-#res = cv2.CreateMat(frame_height, frame_width, cv2.CV_8U)
-
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
 out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width,frame_height))
 
 while(cap.isOpened()):
-    frameSkipped = 15
+    frameSkipped = 1
     prev_frame = frame[:]
     ret, frame = cap.read()
     frameCount += frameSkipped+1
@@ -28,13 +41,16 @@ while(cap.isOpened()):
         im1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #im1 = cv2.GaussianBlur(im1, (21, 21), 0)
         im2 = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+        if first is True:
+            firstFrame = im2
+        im2 = firstFrame
         #im2 = cv2.GaussianBlur(im2, (21, 21), 0)
 
         #im1 = cv2.equalizeHist(im1)
         #im2 = cv2.equalizeHist(im1)
 
         #detect key feature points
-        featureDetectorType = "SIFT"
+        featureDetectorType = "ORB"
         if featureDetectorType is "SIFT":
             detector = cv2.xfeatures2d.SIFT_create()
             kp1 = detector.detect(im1)
@@ -47,10 +63,8 @@ while(cap.isOpened()):
             detector = cv2.ORB_create(nfeatures=1500)
             kp1 = detector.detect(im1)
             kp2 = detector.detect(im2)
-        elif featureDetectorType is "BRISK":
-            detector = cv2.BRISK_create()
 
-        featureDescriptorType = "SIFT"
+        featureDescriptorType = "ORB"
         if featureDescriptorType is "SIFT":
             descriptor = cv2.xfeatures2d.SIFT_create()
             kp1, des1 = detector.compute(im1, kp1)
@@ -64,6 +78,12 @@ while(cap.isOpened()):
             kp1, des1 = detector.compute(im1, kp1)
             kp2, des2 = detector.compute(im2, kp2)
 
+        if first is True:
+            p0 = cv2.goodFeaturesToTrack(im2, mask = None, **feature_params)
+            # Create a mask image for drawing purposes
+            flowMask = np.zeros_like(im2)
+            first = False
+
         #some magic with prev_frame
 
         # BFMatcher with default params
@@ -76,7 +96,7 @@ while(cap.isOpened()):
             # Apply ratio test
             good = []
             for m, n in matchesCurrToPrev:
-                if m.distance < 0.75 * n.distance:
+                if m.distance < 0.5 * n.distance:
                     good.append(m)
 
             points1 = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
@@ -107,14 +127,39 @@ while(cap.isOpened()):
 
 
         # Find homography
-        h, mask = cv2.findHomography(points2, points1, cv2.RANSAC, 3.0)
+        transformationType = "euclidian"
+        if transformationType is "homography":
+            h, mask = cv2.findHomography(points2, points1, cv2.RANSAC, 5.0)
+            height, width = im2.shape
+            im1Reg = cv2.warpPerspective(im1, h, (width, height), flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
+            im2Reg = cv2.warpPerspective(im2, h, (width, height), flags=cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
+        elif transformationType is "euclidian":
+            m = cv2.estimateRigidTransform(points2, points1, fullAffine=False)
+            height, width = im2.shape
+            im1Reg = cv2.warpAffine(im1, m, (width, height))
+            im2Reg = cv2.warpAffine(im2, m, (width, height))
 
-        # Use homography
-        height, width = im2.shape
-        #im1Reg = cv2.warpPerspective(im1, h, (width, height), flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
-        im2Reg = cv2.warpPerspective(im2, h, (width, height), flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
 
+        # calculate optical flow
+        if False:
+            p1, st, err = cv2.calcOpticalFlowPyrLK(im2Reg, im1, p0, None, **lk_params)
 
+            # Select good points
+            good_new = p1[st == 1]
+            good_old = p0[st == 1]
+
+            # draw the tracks
+            for i, (new, old) in enumerate(zip(good_new, good_old)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                flowMask = cv2.line(flowMask, (a, b), (c, d), color[i].tolist(), 2)
+                frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+            flow = cv2.add(im2, flowMask)
+
+            cv2.imshow('flow', flow)
+
+            # Now update the previous frame and previous points
+            p0 = good_new.reshape(-1, 1, 2)
 
         #im2Reg = cv2.GaussianBlur(im2Reg, (21, 21), 0)
         #im1 = cv2.GaussianBlur(im1, (21, 21), 0)
@@ -146,7 +191,9 @@ while(cap.isOpened()):
 
         cv2.imshow("matches.jpg", imMatches)
 
-        cv2.imshow("grayframe", im2Reg)
+        cv2.imshow("im1Reg", im1Reg)
+
+        cv2.imshow("im2Reg", im2Reg)
 
         cv2.imshow("diff", im2Diff)
 
